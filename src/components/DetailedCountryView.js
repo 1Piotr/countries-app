7427e27a9cc64fe5c311f97474fb0a8089b01462
Piotr Borysewicz <piotrborysewicz@yahoo.co.uk>
2023-03-16 12:20:58 +0000

    Update DetailedCountryView.js

--- src/components/DetailedCountryView.js
+++ src/components/DetailedCountryView.js
@@ -26,7 +26,7 @@ const DetailedCountryView = ({countries, countryCode, HandleCountryView, darkMod
             <div className="view">
                 <div>
                     <button className={darkMode ? 'back element dark' : ' back element light'} onClick={Back}>
-                    &#11104; Back</button>
+                    &#8592; Back</button>
                 </div>
                 <div className="countryDetail" style={{display:'flex'}}>   
                     <div style={{display:'flex'}}>
